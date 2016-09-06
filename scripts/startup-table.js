deab274f36c7bafed59dd5643a9ba8dc13e72c54
varunpal <varun.pal@myntra.com>
2016-09-07 00:05:00 +0530

    Added sorting.

--- scripts/startup-table.js
+++ scripts/startup-table.js
@@ -1,18 +1,54 @@
 (function (window) {
   function StartupTable () {
-    this.data = data;
+    var context = this,
+      tableContainer = document.getElementById('tableContainer');
+    this.data = null;
+    tableContainer.addEventListener('change', function (event) {
+      if (event.target.id === 'sort') {
+        context.sortData(parseInt(event.target.value, 10));
+      }
+    });
   }
   
   StartupTable.prototype.render = function (data) {
+    this.data = data;
+    this.renderSortFilter();
+    this.renderTable();
+  }
+
+  StartupTable.prototype.renderTable = function () {
     var context = this,
-      table = document.createElement('table'),
-      container = document.getElementById('tableContainer');
-    data.forEach(function (item, index) {
+      table = document.getElementById('startupTable');
+    table.innerHTML = '';
+    this.data.forEach(function (item, index) {
       var row = context.renderRow(item, index);
       table.appendChild(row);
     });
-    container.innerHTML = '';
-    container.appendChild(table);
+  }
+
+  StartupTable.prototype.sortData = function (index) {
+    var headers = this.data.splice(0, 1);
+    this.data = this.data.sort(function (a, b) {
+      return a[index].toUpperCase() > b[index].toUpperCase() ? 1 : -1;
+    });
+    this.data = headers.concat(this.data);
+    this.renderTable();
+  }
+
+  StartupTable.prototype.renderSortFilter = function () {
+    this.renderSort();
+  }
+
+  StartupTable.prototype.renderSort = function () {
+    var sort = document.getElementById('sort'),
+      columns = this.data[0];
+    sort.innerHTML = '<option selected disabled>Select</option>';
+    columns.forEach(function (column, index) {
+      var option = document.createElement('option');
+      option.value = index;
+      option.innerText = column;
+      sort.appendChild(option);
+    });
   }
 
   StartupTable.prototype.renderRow = function (data, index) {
